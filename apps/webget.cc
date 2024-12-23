1c9b01a5928979067325533bf809636dd5e1bf34
Jlyfish <FishJeli@Outlook.com>
2024-12-23 23:24:58 +0800

    webget implementation

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,17 +8,26 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-
+    TCPSocket socket;
+    socket.connect(Address(host, "host"));
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
 
+    socket.write("GET ." + path + "HTTP/1.1\r\n");
+    socket.write("Host: " + host + "\r\n");
+    socket.write("Connection: close\r\n\r\n");
+
+    while (!socket.eof()) {
+        std::cout << socket.read();
+    }
+    socket.close();
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
