964609f8b9d60a28597ba1f10648f34ca72c5d53
Jlyfish <FishJeli@Outlook.com>
2024-12-24 11:45:37 +0800

    lab0 byte_stream implemenetationm

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,69 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) : buffer_stream(capacity), total_capacity(capacity) {
+
+}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    const size_t put_size = min(total_capacity - current_size, data.size());
+    for (int i = 0; i < put_size; i++) {
+        buffer_stream[tail] = data[i];
+        tail = tail == total_capacity ? 0 : tail + 1; 
+    }
+    current_size += put_size;
+    written_cnt += put_size;
+    return put_size;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    const size_t copy_size = min(current_size, len);
+    string copy_result;
+    for (int i = 0, j = head; i < copy_size; i++) {
+        copy_result += buffer_stream[j];
+        j = j == total_capacity ? 0 : j + 1;
+    }
+    return copy_result;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    const size_t pop_size = min(current_size, len);
+    head += pop_size;
+    if (head >= total_capacity) {
+        head -= total_capacity;
+    }
+    current_size -= pop_size;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    const size_t read_size = min(read_size, len);
+    string read_result;
+    for (int i = 0, j = head; i < read_size; i++) {
+        read_result += buffer_stream[j];
+        j = j == total_capacity ? 0 : j + 1;
+    }
+    pop_output(read_size);
+    read_cnt += read_size;
+    return read_result;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() { input_ended_flag = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return input_ended_flag; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return total_capacity; }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return current_size == 0; }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return input_ended_flag && current_size == 0; }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return written_cnt; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return read_cnt; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return total_capacity - current_size; }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,6 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
+#include <vector>
 
 //! \brief An in-order byte stream.
 
@@ -16,7 +17,13 @@ class ByteStream {
     // all, but if any of your tests are taking longer than a second,
     // that's a sign that you probably want to keep exploring
     // different approaches.
-
+    std::vector<char> buffer_stream;
+    size_t written_cnt{};
+    size_t read_cnt{};
+    size_t total_capacity;
+    size_t current_size{};
+    size_t head{}, tail{};
+    bool input_ended_flag{};
     bool _error{};  //!< Flag indicating that the stream suffered an error.
 
   public:
