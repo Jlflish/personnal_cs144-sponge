542f6bd91589a1d7d3c142f9cccd73827064d337
Jlyfish <FishJeli@Outlook.com>
2024-12-24 16:32:56 +0800

    lab1 stream_reassembler initialize

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,13 +5,16 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
+#include <numeric>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-
+    
+    std::vector<int> _next;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
 
